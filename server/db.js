7720ad5f5869bf763c38eabe07cb0213cc38b282
wontwopunch <jiwontwopunch@gmail.com>
2025-01-20 23:54:24 +0900

    update db.js

---
+++ server/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// DigitalOcean 클러스터에서 제공한 연결 문자열 사용
+const connectionString = 'mongodb+srv://doadmin:5WhxJ7z63820M1qs@db-mongodb-sgp1-31385-27362b68.mongo.ondigitalocean.com/admin?authSource=admin&retryWrites=true&w=majority';
+
+// MongoDB 연결 함수
+const connectDB = async () => {
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
